bea93b34cdddd7e08ea847732afa0370ba8dd8fd
briandgomez <briandgomez98@gmail.com>
2021-09-26 14:27:17 -0400

    Display events

--- script.js
+++ script.js
@@ -6,46 +6,34 @@ currentDate.textContent = moment().format(dateFormat);
 var eventObj = {};
 var timeNow = moment().format("HH");
 
-//Save new event
-$("textarea").on("click", function () {
-
-})
-
+//Retrieves and stores text stored in localStorage
 $(".saveBtn").on('click', function () {
 
     var text = $(this).siblings("textarea").val().trim();
 
     //Returns string of the id (Ex:'hour-9')
     var timeBlockId = $(this).parent().attr('id');
-    var timeBlockNum = parseInt(timeBlockId.slice(-2));
+    var timeBlockNum = parseInt(timeBlockId.match(/\d+/));
 
-    eventObj = {
-        'text': text,
-        'time': timeBlockNum,
-    }
+    localStorage.setItem(timeBlockNum, text);
 
-    localStorage.setItem(timeBlockId, JSON.stringify(eventObj));
-
-    eventDue(timeBlockNum);
+    checkDueDate(timeBlockNum);
 })
 
-var eventDue = function (timeBlockNum) {
-    $('textarea').addClass('past');
+//Adds the apropriate background to each event
+var checkDueDate = function (timeBlockNum) {
     if (timeNow < timeBlockNum) {
         $('#hour-' + timeBlockNum).find('textarea').addClass('future');
-
     }
     else if (timeNow > timeBlockNum) {
         $('#hour-' + timeBlockNum).find('textarea').addClass('past');
     }
     else {
         $('#hour-' + timeBlockNum).find('textarea').addClass('present');
-
     }
-
 }
 
-/*var displayEvents = function () {
-    var storageObj = JSON.parse(localStorage.getItem(eventObj));
-    console.log(storageObj);
-}*/
+//Displays any event in localStorage
+for (var i = 8; i <= 17; i++) {
+    $("#hour-" + i).find('textarea').val(localStorage.getItem(i));
+}
